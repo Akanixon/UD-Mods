3a36a8718a27f93e0cfc52884a588d74e137277e
Qrahn <57023376+Qrahn@users.noreply.github.com>
2020-04-04 15:30:38 +0200

    Update Ammo300mmShell.cs

--- Weapons[WeaponCore]/Data/Scripts/WeaponThread/Ammo300mmShell.cs
+++ Weapons[WeaponCore]/Data/Scripts/WeaponThread/Ammo300mmShell.cs
@@ -90,7 +90,7 @@ namespace WeaponThread
             },
             AreaEffect = new AreaDamageDef
             {
-                AreaEffect = Radiant, // Disabled = do not use area effect at all, Explosive, Radiant, AntiSmart, JumpNullField, JumpNullField, EnergySinkField, AnchorField, EmpField, OffenseField, NavField, DotField.
+                AreaEffect = Explosive, // Disabled = do not use area effect at all, Explosive, Radiant, AntiSmart, JumpNullField, JumpNullField, EnergySinkField, AnchorField, EmpField, OffenseField, NavField, DotField.
                 AreaEffectDamage = 8000f, // 0 = use spillover from BaseDamage, otherwise use this value.
                 AreaEffectRadius = 4.5f,
                 Pulse = new PulseDef // interval measured in game ticks (60 == 1 second), pulseChance chance (0 - 100) that an entity in field will be hit
